82a309d5ed448b78d6ae6cd66dc6ae24385d27eb
EmilPapafrita <emilbaezsalazar@gmail.com>
2023-12-29 09:32:05 -0500

    Added more buttons to navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,14 +4,22 @@ import { HandMetal } from 'lucide-react';
 
 const Navbar = () => {
   return (
-    <div className=' bg-zinc-100 py-2 border-b border-s-zinc-200 fixed w-full z-10 top-0'>
+    <div className=' bg-blue-500 py-2 border-b border-s-zinc-200 fixed w-full z-10 top-0'>
       <div className='container flex items-center justify-between'>
         <Link href='/'>
           <HandMetal />
         </Link>
-        <Link className={buttonVariants()} href='/sign-in'>
-          Sign in
-        </Link>
+        <div>
+          <Link className={buttonVariants()} href='/sign-in'>
+            Sign in
+          </Link>
+          <Link className={buttonVariants()} href='/sign-up'>
+            Sign up
+          </Link>
+          <Link className={buttonVariants()} href='https://www.showtimestrategies.net/'>
+            About
+          </Link>
+        </div>
       </div>
     </div>
   );
