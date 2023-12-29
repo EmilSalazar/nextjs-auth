e734266294310ef544862fd1e46a1d08ffa8d104
EmilPapafrita <emilbaezsalazar@gmail.com>
2023-12-29 16:38:40 -0500

    Finished login and signup

---
+++ src/app/(dashboard)/admin/page.tsx
@@ -0,0 +1,20 @@
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/lib/auth";
+
+const page = async () => {
+    const session = await getServerSession(authOptions);
+    
+    if(session?.user) {
+        return (
+            <h2 className="text-2xl">
+                Admin page - welcome back {session?.user.username}
+            </h2>
+        );
+    }
+
+    return (
+        <h2 className="text-2xl"> Please login to see this admin page</h2>
+    )
+};
+
+export default page;
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,12 @@
 import Link from 'next/link';
-import { buttonVariants } from './ui/button';
+import { Button, buttonVariants } from './ui/button';
 import Image from 'next/image';
+import { authOptions } from '@/lib/auth';
+import { getServerSession } from 'next-auth';
+import UserAccountnav from './UserAccountnav';
 
-const Navbar = () => {
+const Navbar = async () => {
+  const session = await getServerSession(authOptions);
   return (
     <div className=' bg-black py-2 border-b border-s-zinc-200 fixed w-full z-10 top-0'>
       <div className='container flex items-center justify-between'>
@@ -10,12 +14,18 @@ const Navbar = () => {
           <Image src='/logo.png' alt='Logo' width={150} height={150} />
         </Link>
         <div className='flex space-x-4'>
-          <Link className={buttonVariants()}  href='/sign-in'>
-            Sign in
-          </Link>
-          <Link className={buttonVariants()} href='/sign-up'>
-            Sign up
-          </Link>
+          {session?.user ? (
+            <UserAccountnav />
+          ) : (
+            <>
+              <Link className = { buttonVariants() }  href = '/sign-in'>
+                Sign in
+              </Link>
+              <Link className={buttonVariants()} href='/sign-up'>
+                Sign up
+              </Link>
+            </>
+          )}
           <Link className={buttonVariants()} href='https://www.showtimestrategies.net/'>
             About
           </Link>
